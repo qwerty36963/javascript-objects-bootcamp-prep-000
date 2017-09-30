aa8a2a68f33595699f1371be20f6a870d4cd695d
qwerty36963 <sparks21012@gmail.com>
2017-09-30 17:22:34 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,6 @@
+
+var playlist = {artistName: 'song'}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  Object.assign({},playlist,{[Phil Ochs]:"Here's to the State of Mississippi"})
+}
