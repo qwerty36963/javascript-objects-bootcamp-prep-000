819e3062049aacb42ce45bbb0e454b11b4df3a7a
qwerty36963 <sparks21012@gmail.com>
2017-09-30 17:27:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,3 @@
 
-var playlist = {artistName: 'song'}
+var playlist = {Phil Ochs: "Here's to the State of Mississippi"}
 
-function updatePlaylist(playlist, artistName, songTitle){
-  Object.assign({},playlist,{[Phil Ochs]:"Here's to the State of Mississippi"})
-}
