b05cdde74cd0b47fd92b13d5c49b37a8ba4f3444
qwerty36963 <sparks21012@gmail.com>
2017-09-30 18:11:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@
 var playlist = {playlist}
 
 function(playlist, artistName, songTitle){
-playlist.artistName = "Phil Ochs"
-playlist.songTitle = "Here's to the State of Mississippi"
+playlist[artistName] = "Phil Ochs"
+playlist[songTitle] = "Here's to the State of Mississippi"
 return playlist
 }
